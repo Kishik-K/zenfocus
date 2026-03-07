6fcb5a6db359150c450aaf441bb2b6377ce2b587
Kishik <kishikkhajuria03@gmail.com>
2026-03-07 23:03:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let todoList = [
+  { name: 'Welcome to ZenFocus!', dueDate: '2024-02-25', completed: false },
+  { name: 'Check off this task', dueDate: '2024-02-25', completed: false }
+];
 
+// Initial Run
+document.addEventListener('DOMContentLoaded', () => {
+    renderTodoList();
+});
