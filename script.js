0f45c7d39f570c6f591d56937f79e4a24b7028e1
Kishik <kishikkhajuria03@gmail.com>
2026-03-07 23:08:57 +0530

    Added HTML inside the function

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function renderTodoList() {
   const container = document.querySelector('.js-todo-list');
   
   let todoListHTML = '';
+
+todoList.forEach((todo, index) => {
+    todoListHTML += `
+      <div class="todo-row">
+        <div class="checkbox"></div>
+        
+        <div class="todo-info">
+            <span class="todo-name">${todo.name}</span>
+            <span class="todo-date">${todo.dueDate ? '📅 ' + todo.dueDate : 'No due date'}</span>
+        </div>
+
+        <button class="delete-btn">
+            <svg width="20" height="20" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
+            </svg>
+        </button>
+      </div>
+    `;
+  });
+
+  container.innerHTML = todoListHTML;
+}
